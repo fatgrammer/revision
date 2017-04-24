61b783162f96b9f28939dd523554d505586d57b7
fatgrammer <rebornhavei@outlook.com>
2017-04-24 20:44:13 +0800

    push

--- al.h
+++ al.h
@@ -1,6 +1,27 @@
 #ifndef _AL_H_
 #define _AL_H_
-#include <vector>
+template <typename T>
+void print(const T &v) {
+  using namespace std;
+  for (const auto &i : v) {
+    cout << i << " ";
+  }
+  cout << endl;
+}
+
+std::vector<int> gener(size_t size) {
+  std::vector<int> vi;
+  for (int i = 0; i < size; ++i) {
+    vi.push_back(i);
+  }
+
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  std::shuffle(vi.begin(), vi.end(), g);
+  return vi;
+}
+#endif
 struct Al {
   template <typename T>
   static void bubbleSort(std::vector<T> &v) {
@@ -24,26 +45,20 @@ struct Al {
       v[j] = tmp;
     }
   }
+
+  template <typename T>
+  static void selectionSort(std::vector<T> &v) {
+    for (size_t i = 0; i < v.size() - 1; ++i) {
+      // T min = v[i];
+      size_t minIndex = i;
+      for (size_t j = i + 1; j < v.size(); ++j) {
+        if (v[j] < v[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (i != minIndex) {
+        std::swap(v[i], v[minIndex]);
+      }
+    }
+  }
 };
-template <typename T>
-void print(const T &v) {
-  using namespace std;
-  for (const auto &i : v) {
-    cout << i << " ";
-  }
-  cout << endl;
-}
-
-std::vector<int> gener() {
- std::vector<int> vi;
-  for (int i = 0; i < 20; ++i) {
-    vi.push_back(i);
-  }
-
-  std::random_device rd;
-  std::mt19937 g(rd());
-
-  std::shuffle(vi.begin(), vi.end(), g);
-  return vi;
-}
-#endif
--- main.cc
+++ main.cc
@@ -1,16 +1,13 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <vector>
+
+#include <bits/stdc++.h>
 #include "al.h"
 #include "canvas.h"
 using namespace std;
 int main(void) {
-  vector<int> vi = gener();
+  vector<int> vi = gener(20);
   print(vi);
   // Al::bubbleSort(vi);
-  Al::simpleInsertionSort(vi);
+  // Al::simpleInsertionSort(vi);
+  Al::selectionSort(vi);
   print(vi);
 }
