f1249871e75e29b879a5c3242b9b9801aa224d84
fat <rebornhavei@outlook.com>
2017-04-25 17:58:53 +0800

    change main

--- main.cc
+++ main.cc
@@ -2,13 +2,19 @@
 #include "al.h"
 #include "canvas.h"
 using namespace std;
-template <typenaintint main(void) {
-  vector<int> vi = gener(20);
+int main(void) {
+  vector<int> vi = gener(20, false);
   print(vi);
   // rotate(vi.begin(), vi.begin() + 1, vi.end());
   // Al::bubbleSort(vi);
   // Al::simpleInsertionSort(vi);
   // Al::selectionSort(vi);
-  Al::slowSort(vi);
-  print(vi);
+  // Al::slowSort(vi);
+
+  //  next_permutation(vi.begin(), vi.end());
+  //  print(vi);
+  string rep("1122333");
+  auto last = unique(rep.begin(), rep.end());
+  rep.erase(last, rep.end());
+  print(rep);
 }
